09e2ecc09896ace7c4a2fe1785cddf146313c93c
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-28 22:42:23 -0300

    Aprendendo sobre variaveis

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+console.log("Trabalhando com Variáveis"); //Imprimir valor no terminal
+
+//Declarar a variável
+const idade = 26;
+console.log(idade);
+
+//Utilizar operador logico
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade*2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
+const nome = "Lais";
+console.log("nome"); //nome se torna uma string
+console.log(nome);//apresenta valor da variavel
