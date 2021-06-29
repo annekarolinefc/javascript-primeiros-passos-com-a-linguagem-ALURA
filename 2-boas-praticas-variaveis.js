25c1952a61d5ff190af0db87fc5b598b39a359aa
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-28 22:55:44 -0300

    Boas praticas de js

---
+++ 2-boas-praticas-variaveis.js
@@ -0,0 +1,12 @@
+console.log("Boas práticas com Variáveis");
+//JS é case sensitive
+
+idade = 29;
+Idade = 30;
+//idade e Idade sõa variáveis diferentes;
+console.log(idade);
+console.log(Idade);
+
+const idadeUsuario = 20;
+const nome = "Ricardo";
+let ano = 2020;
