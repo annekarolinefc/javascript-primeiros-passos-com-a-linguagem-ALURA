dee6673a9bc16126b9bbef4474423491cf8f5812
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-28 23:10:32 -0300

    Operacoes aritmeticas, conversao de tipos e atribuicao

---
+++ 3-operacoes-aritmeticas.js
@@ -0,0 +1,6 @@
+console.log("Operações Aritmeticas");
+
+console.log(2+2);
+console.log(10+8*2);
+//execução de multiplicação primeiro e depois soma
+console.log((10+8)*2);
---
+++ 5-atribuicao-imutabilidade.js
@@ -0,0 +1,9 @@
+console.log("Atribuição de variáveis");
+const idade = 31;
+const nome = "Ricardo"
+const sobrenome = "Carmo"
+
+console.log(nome, sobrenome);
+console.log(nome + " " + sobrenome);
+
+console.log(`Meu nome é ${nome}`)
