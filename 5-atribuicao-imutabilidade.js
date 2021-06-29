7eccffe66f3ff7d25069a55cc9f0d7b524e88470
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-28 23:13:45 -0300

    Trabalhando imutabilidade

--- 5-atribuicao-imutabilidade.js
+++ 5-atribuicao-imutabilidade.js
@@ -6,4 +6,11 @@ const sobrenome = "Carmo"
 console.log(nome, sobrenome);
 console.log(nome + " " + sobrenome);
 
-console.log(`Meu nome é ${nome}`)
+console.log(`Meu nome é ${nome} ${sobrenome}`)
+
+//ERRO -> nome = nome + sobrenome //sobrescrevendo a variavel
+//console.log(nome); - NÃO É POSSIVEL ATRIBUIR PARA CONST
+
+let nomeNovo = "Ricardo";
+nomeNovo = nomeNovo + " " + sobrenome;
+console.log(nomeNovo);
