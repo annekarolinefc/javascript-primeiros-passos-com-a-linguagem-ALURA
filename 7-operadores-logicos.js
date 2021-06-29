b1e8ab17e68777823cc7b64095e7356b271f9371
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-29 20:46:37 -0300

    Operadores logicos e ifs encadeados

---
+++ 7-operadores-logicos.js
@@ -0,0 +1,8 @@
+const idadeComprador = 15;
+
+if(idade > 18){
+    //executar compra
+    console.log("Comprador maior de idade.")
+} else{
+    console.log("Comprador não é maior de Idade e não poderá realizar uma compra")
+}
---
+++ 8-ifs-encadeados.js
@@ -0,0 +1,13 @@
+const idadeComprador = 15;
+
+if(idade > 18){
+    //executar compra
+    console.log("Comprador maior de idade.")
+} else{
+    //a pessoa é menor de idade
+    if(estaAcompanhada){
+        console.log("Comprador está acomoanhado")
+    }else{
+    console.log("Comprador não é maior de Idade e não poderá realizar uma compra")
+    } 
+}
